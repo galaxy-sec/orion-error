4952f40c8c087b8bc4f45826f6a0013b6bd523ed
Ubuntu <ubuntu@localhost.localdomain>
2025-03-18 14:23:35 +0800

    fix

--- src/core/stc_impl.rs
+++ src/core/stc_impl.rs
@@ -2,7 +2,7 @@ use std::fmt::Display;
 
 use super::{
     domain::{DomainFrom, DomainReason},
-    error::{StructError, StructErrorTrait, StructReason, ste_from_uvs},
+    error::{StructError, StructReason, ste_from_uvs},
     universal::{ConfRSEnum, ErrorPayload, UvsReason, UvsReasonFrom},
 };
 
@@ -43,19 +43,6 @@ where
     }
 }
 
-pub fn stc_err_from<R1, R2>(error: StructError<R1>) -> StructError<R2>
-where
-    R1: DomainReason + Clone,
-    R2: DomainReason,
-    R2: From<R1>,
-{
-    let reason = match error.get_reason().clone() {
-        StructReason::Universal(uvs) => StructReason::Universal(uvs),
-        StructReason::Domain(domain) => StructReason::Domain(R2::from(domain)),
-    };
-
-    StructError::new(reason)
-}
 impl<R: DomainReason> DomainFrom<R, R> for StructError<R> {
     fn from_domain(reason: R) -> StructError<R> {
         StructError::new(StructReason::Domain(reason))
@@ -82,33 +69,5 @@ where
     fn from_domain(value: (R, E)) -> StructError<R> {
         let detail = format!("{}", value.1);
         StructError::new(StructReason::Domain(value.0)).with_detail(detail)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::{DomainReason, StructError, stc_err_from};
-
-    #[derive(Debug, PartialEq, Clone)]
-    struct ReasonA {}
-
-    impl DomainReason for ReasonA {}
-    #[derive(Debug, PartialEq)]
-    struct ReasonB {}
-    impl DomainReason for ReasonB {}
-
-    impl From<ReasonA> for ReasonB {
-        fn from(_value: ReasonA) -> Self {
-            ReasonB {}
-        }
-    }
-
-    #[test]
-    fn test_conv() {
-        type ErrorA = StructError<ReasonA>;
-        type ErrorB = StructError<ReasonB>;
-        let e_a = ErrorA::from(ReasonA {});
-        let e_b: ErrorB = stc_err_from(e_a);
-        println!("e_b: {:?}", e_b)
     }
 }
