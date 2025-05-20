8d5818c185930a897218f2836cd9d15ce2bd8c3b
wukong <>
2025-05-20 09:37:30 +0800

    cargo clippy

--- src/core/reason.rs
+++ src/core/reason.rs
@@ -1,6 +1,3 @@
-/// Represents the root cause of an error, which can be either
-/// a domain-specific reason or a universal system reason.
-
 pub trait ErrorCode {
     fn error_code(&self) -> i32 {
         500
